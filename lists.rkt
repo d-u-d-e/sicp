ed02d457968261b886a18d79bb0753067b171252
mmariotto <marco.mariotto@texa.com>
2025-10-03 11:42:59 +0200

    feat: powerset using lists

--- lists.rkt
+++ lists.rkt
@@ -8,6 +8,7 @@
 
 (list-ref (list 1 2 3 4) 2)
 
+; ex 2.18
 ; reverse a list
 (define (reverse l)
   (define (helper l acc)
@@ -18,6 +19,7 @@
 
 (reverse (list 1 2 3 4))
 
+; ex 2.23
 ; for-each
 (define (for-each proc l)
   (cond
@@ -27,3 +29,14 @@
 (for-each
  (lambda (x) (display x) (newline))
  (list 7 4 5 6))
+
+; ex 2.32
+; the power set of a list of elements
+(define (subsets l)
+  (if (null? l)
+      (list nil)
+      (let ((rest (subsets (cdr l))))
+        (append rest (map (lambda (x)
+                            (cons (car l) x))
+                          rest)))))
+(subsets (list 1 2 3 4))
