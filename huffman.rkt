06e097e92682c9d0bf4ee926ec635d3124d50462
mmariotto <marco.mariotto@texa.com>
2025-10-06 16:30:32 +0200

    feat: updated huffman example

--- huffman.rkt
+++ huffman.rkt
@@ -112,3 +112,20 @@
 (define huffman-tree (generate-huffman-tree sample-pairs))
 (equal? (decode (encode message huffman-tree) sample-tree) message)
 (equal? (decode (encode message sample-tree) huffman-tree) message)
+
+; ex 2.70
+(define pairs
+  (list (list 'A 2) (list 'GET 2) (list 'SHA 3) (list 'WAH 1) (list 'BOOM 1)
+        (list 'JOB 2) (list 'NA 16) (list 'YIP 9))
+  )
+
+(set! huffman-tree (generate-huffman-tree pairs))
+(set! message (list 'GET 'A 'JOB
+                    'SHA 'NA 'NA 'NA 'NA 'NA 'NA 'NA 'NA
+                    'GET 'A 'JOB
+                    'SHA 'NA 'NA 'NA 'NA 'NA 'NA 'NA 'NA
+                    'WAH 'YIP 'YIP 'YIP 'YIP 'YIP 'YIP 'YIP 'YIP 'YIP
+                    'SHA 'BOOM))
+
+(display "Bits used with huffman encoding: ") (length (encode message huffman-tree))
+(display "Bits used with a 3-bit fixed length code: ") (* 3 (length message))
