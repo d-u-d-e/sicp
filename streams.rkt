69e699499657e50b185f8765db25d8d579a6d8bc
mmariotto <marco.mariotto@texa.com>
2025-10-13 17:33:30 +0200

    feat: streams examples

--- streams.rkt
+++ streams.rkt
@@ -13,6 +13,31 @@
       (cons (stream-car stream) (stream-take (stream-cdr stream) (- n 1)))
       nil))
 
-(define s (stream-enumerate-interval 1 1000))
-(define a (stream-take s 3))
-(for-each (lambda (x) (display x) (newline)) a)
+;(define s (stream-enumerate-interval 1 1000))
+;(define a (stream-take s 3))
+;(for-each (lambda (x) (display x) (newline)) a)
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+
+; ex 3.51
+(define (show x)
+  (display x) (newline) x)
+
+(define x
+  (stream-map show (stream-enumerate-interval 0 10)))
+
+; stream-ref prints all integers up to 5 and then returns 5
+(stream-ref x 5)
+; stream-ref here won't reprint all integers up to 5 because we already forced
+; a part of the stream; as before it prints 7 twice
+(stream-ref x 7)
